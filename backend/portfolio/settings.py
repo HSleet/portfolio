4c24fbbc14c02b795057568eec4c802996276298
HSleet <47047283+HSleet@users.noreply.github.com>
2023-12-18 02:27:59 -0400

    Update settings.py

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '') != 'False'
 ALLOWED_HOSTS = ['*']
-
+ADMIN_ENABLED = True
 
 # Application definition
 
