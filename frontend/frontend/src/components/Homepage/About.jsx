cb858eb195b759276d59c72e8b109dc682ca5d24
HSleet <47047283+HSleet@users.noreply.github.com>
2023-12-22 14:04:45 -0400

    Update About.jsx
    clean up

--- frontend/frontend/src/components/Homepage/About.jsx
+++ frontend/frontend/src/components/Homepage/About.jsx
@@ -7,9 +7,7 @@ const About = ({isLightTheme, shortBio}) => {
 
     return (
         <div className='flex flex-col'>
-            <p className={`pt-20 pb-10 text-left font-sans text-lg ${isLightTheme ? 'text-light-body-text2' : 'text-dark-body-text1'} antialiased`} style={{ transition: 'color 0.5s ease' }}>{shortBio}
-                {/* I'm a passionate software engineer boasting three years of hands-on experience ranging from meticulous QA automation and adept bug fixing to the seamless migration of software to advanced versions. My development journey has been shaped by a fusion of technical proficiency and creativity. Rooted in a foundation of electronics engineering, my journey into programming started as a hobby fueled by passion, and naturally, it has shaped my professional path ever since. */}
-            </p>
+            <p className={`pt-20 pb-10 text-left font-sans text-lg ${isLightTheme ? 'text-light-body-text2' : 'text-dark-body-text1'} antialiased`} style={{ transition: 'color 0.5s ease' }}>{shortBio}</p>
             <div className='nav-buttons flex flex-col sm:flex-row justify-center items-center'>
                 <div className={`button-container ${isLightTheme ? 'bg-light-body-button': 'bg-dark-body-button'}`}>
                     <Link to='/projects'>
